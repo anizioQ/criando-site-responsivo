03b532bb75f4c16974eec2a833a9d7d59f4b79a0
anizioQ <anizioq@gmail.com>
2020-05-15 17:02:34 -0300

    salvando arquivos

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+// task para o sass
+gulp.task('sass', function(){
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) // Using gulp-sass
+        .pipe(gulp.dest('css'))
+});
+
+// task para o watch
+gulp.task('watch', function(){
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
+});
+
+// task default gulp
+gulp.task('default', gulp.series('sass', 'watch'));
