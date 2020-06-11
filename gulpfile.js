6e734ee1655d8640b19a9d2382a5a81cd6689e53
anizioQ <anizioq@gmail.com>
2020-06-11 00:00:21 -0300

    Bootstrap-Fontawesome-GoogleFonts configurados

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,32 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
-// compilar o SASS
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src('scss/**/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) // Using gulp-sass
-        .pipe(gulp.dest('css'))
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'scss/**/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
 });
 
-// mover JS para /js
-/* gulp.task('js', function() {
-    return gulp.src("/node_modules/jquery/dist/jquery.min.js", "/node_modules/popper.js/dist/popper.min.js", "/node_modules/bootstrap/dist/js/bootstrap.min.js")
-            .pipe(gulp.dest('js'))
-}); */
-
-// escutar pastas com SCSS
-gulp.task('watch', function() {
-    gulp.watch('scss/**/*.scss', gulp.series('sass'));
+// Move the javascript files into our /src/js folder
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("js"))
+        .pipe(browserSync.stream());
 });
 
-// task default gulp
-gulp.task('default', gulp.series('sass', 'watch'));
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.series('sass', function() {
+
+    browserSync.init({
+        server: "./"  
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'scss/**/*.scss'], gulp.series('sass'));
+    gulp.watch("*.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.parallel('js','serve'));
+
